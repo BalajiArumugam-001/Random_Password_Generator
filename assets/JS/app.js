562b79def50c26797060b862b6d255f4524b38ba
Balaji Arumugam <57753452+BalajiArumugam-001@users.noreply.github.com>
2023-03-09 17:55:59 +0530

    Add files via upload

---
+++ assets/JS/app.js
@@ -0,0 +1,11 @@
+
+/*get the number of characters*/
+function charLength() {
+
+    let charLength = document.getElementById('length').value;
+    document.getElementById('result').innerHTML = charLength;
+    return charLength;
+
+}
+
+document.getElementById('length').addEventListener('change', charLength)
